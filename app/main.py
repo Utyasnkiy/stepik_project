fba9719d784fdbe92f72270f01b5367a124cbe63
Utyanskiy <25072002d@mail.ru>
2025-08-21 10:03:53 +0300

    добавлены фильтры

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel, ValidationError
+import asyncio
+from app.database import sample_products
 
 from app.config import load_config
 from app.logger import logger
@@ -70,42 +72,12 @@ fake_db = [
 #     return new_user
 
 
-sample_product_1 = {
-    "product_id": 123,
-    "name": "Smartphone",
-    "category": "Electronics",
-    "price": 599.99
-}
 
-sample_product_2 = {
-    "product_id": 456,
-    "name": "Phone Case",
-    "category": "Accessories",
-    "price": 19.99
-}
 
-sample_product_3 = {
-    "product_id": 789,
-    "name": "Iphone",
-    "category": "Electronics",
-    "price": 1299.99
-}
-
-sample_product_4 = {
-    "product_id": 101,
-    "name": "Headphones",
-    "category": "Accessories",
-    "price": 99.99
-}
-
-sample_product_5 = {
-    "product_id": 202,
-    "name": "Smartwatch",
-    "category": "Electronics",
-    "price": 299.99
-}
-
-sample_products = [sample_product_1, sample_product_2, sample_product_3, sample_product_4, sample_product_5]
+@app.get("/")
+async def read_root():
+    await asyncio.sleep(10)  # эмуляция длительной операции
+    return {"message": "Hello, World!"}
 
 
 @app.get('/product')
@@ -120,3 +92,16 @@ async def get_product_by_id(id: int):
         if el['product_id'] == id:
             return el
     return {"message": "Product not found((01001))"}
+
+
+@app.get('/products/search')
+async def get_product_filter(keyword:str= '', category:str = None, limit:int=None):
+    data = list(sample_products)
+    if category != None:
+        data = [el for el in data if el['category']==category]
+    result = []
+    for el in data:
+    
+        if keyword.lower() in el['name'].lower():
+            result.append(el)
+    return result[:limit]
